50925104a0c07e1c3d07dafee31879e806ac15f4
Salih Ozturk <salihozturk@yahoo.com>
2022-10-11 20:18:59 +0200

    Add GetByUserIdUserOperationClaimQuery, refactor mapping configuration

--- src/kodlamaIoDevs/Application/Features/UserOperationClaims/Profiles/MappingProfiles.cs
+++ src/kodlamaIoDevs/Application/Features/UserOperationClaims/Profiles/MappingProfiles.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.UserOperationClaims.Commands.CreateUserOperationClaim;
 using Application.Features.UserOperationClaims.Dtos;
+using Application.Features.UserOperationClaims.Models;
 using AutoMapper;
+using Core.Persistence.Paging;
 using Core.Security.Entities;
 
 namespace Application.Features.UserOperationClaims.Profiles
@@ -13,6 +15,16 @@ namespace Application.Features.UserOperationClaims.Profiles
             CreateMap<UserOperationClaim, CreatedUserOperationClaimDto>().ReverseMap();
 
             CreateMap<UserOperationClaim, DeletedUserOperationClaimDto>().ReverseMap();
+
+            CreateMap<UserOperationClaim, GetByUserIdUserOperationClaimDto>()
+                .ForMember(c => c.OperationClaimName, opt => opt.MapFrom(c => c.OperationClaim.Name))
+                .ReverseMap();
+
+            CreateMap<IPaginate<UserOperationClaim>, GetUserOperationClaimListModel>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(source => source.Items.Select(uoc => uoc.UserId).First()))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(source => source.Items.Select(uoc => uoc.User.Email).First()));
+
+
         }
     }
 }
