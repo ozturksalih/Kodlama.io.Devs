7af0957f237ee685c6eabad0a1c73795909181f5
Salih Ozturk <salihozturk@yahoo.com>
2022-10-16 14:47:48 +0200

    Add register and login validator

---
+++ src/kodlamaIoDevs/Application/Features/Auths/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Auths.Commands.Login
+{
+    public class LoginCommandValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginCommandValidator()
+        {
+            RuleFor(u => u.UserForLoginDto.Email).NotEmpty();
+            RuleFor(u => u.UserForLoginDto.Email).NotEqual("string");
+            RuleFor(u => u.UserForLoginDto.Email).Matches("@").WithMessage("Entered email is not a email!");
+        }
+    }
+}
---
+++ src/kodlamaIoDevs/Application/Features/Auths/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Auths.Commands.RegisterMember;
+using FluentValidation;
+
+namespace Application.Features.Auths.Commands.Register
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+
+            RuleFor(u => u.UserForRegisterDto.Email).NotEmpty();
+            RuleFor(u => u.UserForRegisterDto.Email).NotEqual("string");
+            RuleFor(u => u.UserForRegisterDto.Email).Matches("@").WithMessage("Entered email is not a email!");
+            RuleFor(u => u.UserForRegisterDto.Password)
+                .NotEmpty().WithMessage("Entered Password is empty!")
+                .MinimumLength(8).WithMessage("Entered password doesn't meet required minimum length!")
+                .Matches("[A-Z]").WithMessage("Password must have at least one Uppercase Letter")
+                .Matches("[a-z]").WithMessage("Password must have at least one Lowercase Letter")
+                .Matches("[0-9]").WithMessage("Password must have at least one number")
+                .Matches("[^a-zA-Z0-9]").WithMessage("Password must have at least one special character");
+            RuleFor(u => u.UserForRegisterDto.FirstName).NotEmpty();
+            RuleFor(u => u.UserForRegisterDto.LastName).NotEmpty();
+        }
+    }
+}
